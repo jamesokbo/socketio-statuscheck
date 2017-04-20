a8fd1ae47f5b1266b713eafb5fa3708a7a14d221
Jaime Okhuysen <jamesokbo@gmail.com>
2017-04-20 22:07:44 +0000

    debugged version

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 var async = require('async');
 var timeoutCallback = require('timeout-callback');
 
-
 module.exports = function(socket, type, loopTimeout,socketTimeout){
     var status=true;
     
     if(!loopTimeout){
-        loopTimeout=5000;
+        loopTimeout=10000;
     }
     if(!socketTimeout){
-        socketTimeout=5000;
+        socketTimeout=10000;
     }
     
     if(type=="client"){
         socket.on('statuscheck',function(fn){
-            fn(true);
+            fn(null,true);
         });
     }
     else if(type=="server"){
         async.whilst(
             function(){return status},
             function(callback){
-                socket.emit('statuscheck',timeoutCallback(socketTimeout,function(err,res){
-                    if(err){
-                        socket.disconnect();
-                        status=false;
-                    }
-                    console.log("socket connected: "+res);
-                    callback();
-                }));
+                setTimeout(function(){
+                    socket.emit('statuscheck',timeoutCallback(socketTimeout,function(err,res){
+                        if(err){
+                            socket.disconnect();
+                            status=false;
+                        }
+                        callback();
+                    }));
+                },loopTimeout);
             }
         );
     }
